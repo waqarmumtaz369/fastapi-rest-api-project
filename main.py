bcb3b74227fb6e38677db7e6973f14269cec4b4b
waqarmumtaz369 <waqar.mumtaz@gmail.com>
2024-06-02 11:51:51 +0500

    Add UserUpdateRequest model class and PUT method to update the user

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from typing import List
-from models import User, Gender, Role
+from models import User, Gender, Role, UserUpdateRequest
 from uuid import UUID
 
 
@@ -105,6 +105,28 @@ async def delete_user(user_id: UUID):
         if user.id == user_id:
             db.remove(user)
             return
+    raise HTTPException(
+        status_code= 404,
+        detail=f"User with id:{user_id} does not exist."
+    )
+
+# Create a method to Update the User
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.gender is not None:
+                user.gender = user_update.gender
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+        
     raise HTTPException(
         status_code= 404,
         detail=f"User with id:{user_id} does not exist."
